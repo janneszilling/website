355ee38d159a97b73f8a27b7446f3156c42ff0cf
Jannes Zilling <jannes.zilling@gmail.com>
2020-03-09 12:17:07 +0100

    Add files via upload

---
+++ assets/js/hoverEffect.js
@@ -0,0 +1,17 @@
+new hoverEffect({
+    parent: document.querySelector(".img--01"),
+    intensity: 0.5,
+    image1: "../assets/img/photo_01.png",
+    image2: "../assets/img/photo_02.png",
+    imagesRatio: 1100 / 1000,
+    displacementImage: "../assets/img/dis.png",
+  });
+
+new hoverEffect({
+    parent: document.querySelector(".img--02"),
+    intensity: 0.5,
+    image1: "../assets/img/photo_03.png",
+    image2: "../assets/img/photo_04.png",
+    imagesRatio: 1100 / 1000,
+    displacementImage: "../assets/img/dis.png",
+  });
